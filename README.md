20582ae34c5e69eba791efddf37e6e5e05fdf10f
= <danielstorey@gmail.com>
2022-03-08 16:36:45 +0000

    update automatic global registrations and README files

---
+++ README.md
@@ -0,0 +1,27 @@
+# leviate
+
+A tool for building Single Page Applications to be hosted on the Host platform - aka Host Plugins.
+
+In essence this module can be broken down into three separate parts:
+
+- a CLI for initialising apps and running helpful utility functions
+- a project template
+- core files consisting of
+  - essential application files used within all host plugins
+  - additional extensions which are often but not always used
+
+## Installation
+
+```
+// Instructions to follow
+```
+
+## Usage
+
+### CLI
+
+Instructions to follow
+
+### Template
+
+Instructions are included in the template's [README.md](template/README.md)
--- template/project/src/main.js
+++ template/project/src/main.js
@@ -1,15 +1,9 @@
-// Required imports
-import Vue from 'vue';
 import { createApp } from 'leviate';
 
 // Project imports
 import VDragged from 'v-dragged';
 import ShortKey from 'vue-shortkey';
-import globalize from './components/ui';
 
-globalize();
-Vue.use(ShortKey);
-Vue.use(VDragged);
 
 const appConfig = {
   globalConfig: {
@@ -21,10 +15,7 @@ const appConfig = {
   },
   plugins: [
     ShortKey, VDragged,
-    // You can also pass options using an array e.g. [MyPlugin, pluginConfig]
-  ],
-  components: [
-    // components to be included globally e.g. CustomInput, CustomView etc.
+    // You can also pass options using an array e.g. [MyPlugin, myPluginConfig]
   ],
 };
 
