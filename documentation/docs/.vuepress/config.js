26c417cd49c33d969b550157c50a9e78412a5854
Calum Hainsworth <calum.hainsworth@leviat.com>
2022-04-29 15:06:47 +0100

    Added Sidebar and sutomatic linking of l2 headers in documents. Added MD files for basic structure

--- documentation/docs/.vuepress/config.js
+++ documentation/docs/.vuepress/config.js
@@ -1,4 +1,14 @@
+
 module.exports = {
     title: 'Leviate Documentation',
     description: 'Documentation for Leviate',
+    themeConfig: {
+        displayAllHeaders: true,
+        sidebar: [
+            '/getting-started',
+            '/CLI',
+          '/project-template',
+          '/core'
+        ]
+      }
   }
---
+++ documentation/docs/core.md
@@ -0,0 +1,8 @@
+# Core
+
+## Overview
+## Directives directory
+## Extensions directory
+## Plugins directory
+## Scripts directory
+## Store directory
