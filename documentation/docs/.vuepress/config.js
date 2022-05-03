c1fc830c5c8c7cea1630307249eb435a2ff75ec3
Calum Hainsworth <calum.hainsworth@leviat.com>
2022-05-03 09:10:35 +0100

    corrected indentation, removed unnecesary main form package.json and improved module name

--- documentation/docs/.vuepress/config.js
+++ documentation/docs/.vuepress/config.js
@@ -1,14 +1,13 @@
-
 module.exports = {
     title: 'Leviate Documentation',
     description: 'Documentation for Leviate',
     themeConfig: {
-        displayAllHeaders: true,
-        sidebar: [
-            '/getting-started',
-            '/CLI',
-          '/project-template',
-          '/core'
-        ]
-      }
+      displayAllHeaders: true,
+      sidebar: [
+        '/getting-started',
+        '/CLI',
+        '/project-template',
+        '/core'
+      ]
+    }
   }
