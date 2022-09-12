f42888f00670a946f2b985ac2e004fe067943b7d
Daniel Storey <danielstorey@gmail.com>
2022-09-12 17:14:24 +0100

    chore(cli): minor update to cli commands

--- cli/commands/version.js
+++ cli/commands/version.js
@@ -2,6 +2,7 @@ import logger from '../logger';
 import { getGlobalVersion, getLocalVersion } from '../helpers';
 
 export default {
+  usage: 'run [-g]',
   async run(options) {
     const isGlobal = options.global || Object.values(options).includes('-g');
     const version  = isGlobal ? getGlobalVersion() : getLocalVersion();
