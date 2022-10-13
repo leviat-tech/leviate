1232db33288f1e99e3fc1aa008426b4a1f706d7f
robiharid <rh@robx.tech>
2022-10-13 12:30:31 +0100

    chore: formatting

--- core/createApp.js
+++ core/createApp.js
@@ -3,22 +3,22 @@ import Concrete from '@crhio/concrete';
 import HostPlugin, { useHost } from './plugins/host';
 import { createStore, initializeStore } from './store';
 import { createRouter } from './router.js';
-import concreteDefaultOptions from './concreteOptions'
-import './assets/styles/index.css'
+import concreteDefaultOptions from './concreteOptions';
+import './assets/styles/index.css';
 
 function installPlugins(
   app,
   { concreteOptions, endpoints, locales, plugins, globalConfig }
 ) {
-  plugins?.forEach((plugin) => loadPlugin(app, plugin))
+  plugins?.forEach((plugin) => loadPlugin(app, plugin));
 
-  const concreteConfig = { ...concreteDefaultOptions, ...concreteOptions }
-  app.use(Concrete, concreteConfig)
-  app.use(HostPlugin, { endpoints, locales })
+  const concreteConfig = { ...concreteDefaultOptions, ...concreteOptions };
+  app.use(Concrete, concreteConfig);
+  app.use(HostPlugin, { endpoints, locales });
 
   if (globalConfig) {
-    app.config.globalProperties.$config = globalConfig
-    app.provide('$config', globalConfig)
+    app.config.globalProperties.$config = globalConfig;
+    app.provide('$config', globalConfig);
   }
 }
 
