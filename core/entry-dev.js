ae5fadbafa615e6283d9ae608ccb1855aaa1b5da
Daniel Storey <danielstorey@gmail.com>
2022-11-10 18:04:07 +0000

    fix: simplify entry point and make app/store globally available for e2e testing

--- core/entry-dev.js
+++ core/entry-dev.js
@@ -1,12 +1,36 @@
 import baseConfig from './base.config';
 import appConfig from '@/leviate.config';
-import mockConfig from '@/mock.config';
 import { createApp } from './createApp';
-import Dev from './components/Dev.vue';
-import { useMock } from './host-mock';
 
-const onAppCreated = () => useMock(import.meta.env.VITE_PROXY_ACCESS_TOKEN, mockConfig, baseConfig.locales);
 
-const projectConfig = { ...baseConfig, ...appConfig, onAppCreated }
+const isStandalone = window.self === window.top;
 
-createApp(projectConfig, Dev);
+async function getAppcreatedHandler() {
+  if (!isStandalone) return null;
+
+  if (isStandalone) {
+    const mockConfig = await import('@/mock.config');
+    const { useMock } = await import('./host-mock');
+    return () => {
+      useMock(import.meta.env.VITE_PROXY_ACCESS_TOKEN, mockConfig.default, baseConfig.locales);
+    };
+  }
+}
+
+async function getRootComponent() {
+  const rootComponentModule = isStandalone
+    ? await import('./components/Dev.vue')
+    : await import('./components/App.vue');
+
+  return rootComponentModule.default;
+}
+
+async function start() {
+  const onAppCreated = await getAppcreatedHandler();
+  const rootComponent = await getRootComponent();
+  const projectConfig = { ...baseConfig, ...appConfig, onAppCreated };
+
+  createApp(projectConfig, rootComponent, isStandalone);
+}
+
+start();
