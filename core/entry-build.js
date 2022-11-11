509800e882d5f9291f59b63d5c6f6fec5b818bba
Daniel Storey <danielstorey@gmail.com>
2022-11-11 09:56:13 +0000

    chore: refactor entry point in core and template

--- core/entry-build.js
+++
@@ -1,8 +0,0 @@
-import baseConfig from './base.config';
-import appConfig from '@/leviate.config';
-import { createApp } from './createApp';
-import App from './components/App.vue';
-
-const projectConfig = { ...baseConfig, ...appConfig }
-
-createApp(projectConfig, App);
