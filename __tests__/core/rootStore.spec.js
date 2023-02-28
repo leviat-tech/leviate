fcb39bd0e359d7cd98e60525134fecd9df220208
Daniel Storey <danielstorey@gmail.com>
2023-02-28 17:37:50 +0000

    feat: add useApiGateway to core
    chore: replace jest with vitest

--- __tests__/core/rootStore.spec.js
+++ __tests__/core/rootStore.spec.js
@@ -1,10 +1,14 @@
 import { createPinia, defineStore, setActivePinia } from 'pinia';
-import { useRootStore } from '../../core/store/index';
+import { createStore, useRootStore } from '@/core/store';
 
+const testStoreConfig = {
+  modules: [],
+};
 let rootStore;
 
 beforeAll(async() => {
   setActivePinia(createPinia());
+  createStore(testStoreConfig);
   rootStore = useRootStore();
 });
 
@@ -31,7 +35,7 @@ describe('Root store', () => {
     it('registers a module', () => {
       rootStore._registerModule(useFooStore);
 
-      expect(rootStore.foo.value).toBe(1)
+      expect(rootStore.modules.foo().value).toBe(1)
     });
 
     it('maintains reactivity on a registered module', () => {
@@ -39,7 +43,7 @@ describe('Root store', () => {
       const fooStore = useFooStore();
       fooStore.setFoo(2);
 
-      expect(rootStore.foo.value).toBe(2)
+      expect(rootStore.modules.foo().value).toBe(2)
     });
 
   });
@@ -67,13 +71,13 @@ describe('Root store', () => {
       fooStore.setFoo(0);
 
       expect(useFooStore().value).toBe(0);
-      expect(rootStore.foo.value).toBe(0);
+      expect(rootStore.modules.foo().value).toBe(0);
     });
 
     it('serialises the state', () => {
       rootStore._registerModule(useFooStore);
 
-      const state = rootStore.serializeState();
+      const state = rootStore.toJSON();
 
       expect(state.foo.value).toBe(0);
       expect(typeof JSON.stringify(state)).toBe('string');
@@ -84,11 +88,11 @@ describe('Root store', () => {
 
     it('should update the state within a transaction', () => {
       const result = rootStore.transact(() => {
-        rootStore.foo.setFoo(50);
+        rootStore.modules.foo().setFoo(50);
       });
 
       expect(result).toBeInstanceOf(Promise);
-      expect(rootStore.foo.value).toEqual(50);
+      expect(rootStore.modules.foo().value).toEqual(50);
     });
 
     it('should throw an error in a synchronous transaction', () => {
@@ -108,7 +112,7 @@ describe('Root store', () => {
     });
 
     it('should throw an error in an asynchronous transaction', () => {
-      jest.spyOn(console, 'error').mockImplementation(jest.fn());
+      vi.spyOn(console, 'error').mockImplementation(vi.fn());
 
       const transactionWithError = () => rootStore.transact(async () => {
         throw new Error();
@@ -120,32 +124,30 @@ describe('Root store', () => {
     it('should undo the revision if the transaction fails', async () => {
       // We're expecting a console.error but don't
       // want to see it in the test runner logs
-      jest.spyOn(console, 'error').mockImplementation(jest.fn());
+      vi.spyOn(console, 'error').mockImplementation(vi.fn());
 
       // Spy on store revision so we can check it gets called
-      // const undoSpy = jest.spyOn(store.revision, 'undo');
+      // const undoSpy = vi.spyOn(store.revision, 'undo');
 
-      rootStore.foo.setFoo(1);
-      rootStore.foo.setFoo(2);
+      rootStore.modules.foo().setFoo(1);
+      rootStore.modules.foo().setFoo(2);
 
       // Setup a callback that commits a mutation but throws an error
       // So we can check the mutation gets reverted
       const callbackWithError = () => {
-        rootStore.foo.setFoo(3);
+        rootStore.modules.foo().setFoo(3);
 
         return new Promise((resolve, reject) => {
-          rootStore.foo.setFoo(4);
+          rootStore.modules.foo().setFoo(4);
 
-          setTimeout(() => {
-            reject();
-          }, 1000);
+          setTimeout(reject, 1000);
         });
       };
 
       // await rootStore.transact(callbackWithError);
 
       expect(callbackWithError).rejects.toThrowError();
-      expect(rootStore.foo.value).toBe(4);
+      expect(rootStore.modules.foo().value).toBe(4);
 
       // expect(store.revision.undos.length).toBe(0);
       // expect(undoSpy).toHaveBeenCalled();
