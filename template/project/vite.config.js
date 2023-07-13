4b5954ae53893789faf2c53cb72aa2e5168d0287
kwasilonek <you@example.com>
2023-07-13 16:33:40 +0200

    fix: move logic to server folder

--- template/project/vite.config.js
+++ template/project/vite.config.js
@@ -2,48 +2,9 @@ import path from 'path';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import svgLoader from 'vite-svg-loader';
+import getDefaultTemplateCompilerOptions from '@crhio/leviate/server/defaultTemplateCompilerOptions.js'
+
 import 'dotenv/config';
-
-const PRODUCTION_MODE = 'production'
-
-export function removeDataTestAttrs(node) {
-  const htmlNodeTypes = {
-    element: 1,
-    attribute: 6
-  };
-
-  const attributesToRemove = ['data-cy', ':data-cy'];
-
-  const isElement = node.type === htmlNodeTypes.element;
-
-  if (isElement) {
-    node.props = node.props.filter(function (prop) {
-      const isAttribute = prop.type === htmlNodeTypes.attribute;
-
-      if (isAttribute) {
-        const attributeName = prop.name;
-        return !attributesToRemove.includes(attributeName);
-      }
-
-      const isDynamicAttribute = prop.name === 'bind';
-
-      if (isDynamicAttribute) {
-        const attributeName = prop.arg?.content;
-        return !attributesToRemove.includes(attributeName);
-      }
-
-      return true;
-    });
-  }
-}
-
-export function getDataCyTemplateOptions(mode) {
-  return {
-    compilerOptions: {
-      nodeTransforms: mode === PRODUCTION_MODE ? [removeDataTestAttrs] : [],
-    },
-  }
-}
 
 export default defineConfig(({ mode }) => ({
   resolve: {
@@ -70,7 +31,7 @@ export default defineConfig(({ mode }) => ({
   },
 
   plugins: [
-    vue({ template: getDataCyTemplateOptions(mode) }),
+    vue(getDefaultTemplateCompilerOptions(mode)),
     svgLoader(),
   ],
 
