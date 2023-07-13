8040d6b7d8701f2373bd4a9ee9e0b27687a32893
kwasilonek <you@example.com>
2023-07-13 14:01:56 +0200

    fix: remove data-cy from production code

--- template/project/vite.config.js
+++ template/project/vite.config.js
@@ -4,7 +4,48 @@ import vue from '@vitejs/plugin-vue';
 import svgLoader from 'vite-svg-loader';
 import 'dotenv/config';
 
-export default defineConfig({
+const PRODUCTION_MODE = 'production'
+
+export function removeDataTestAttrs(node) {
+  const htmlNodeTypes = {
+    element: 1,
+    attribute: 6
+  };
+
+  const attributesToRemove = ['data-cy', ':data-cy'];
+
+  const isElement = node.type === htmlNodeTypes.element;
+
+  if (isElement) {
+    node.props = node.props.filter(function (prop) {
+      const isAttribute = prop.type === htmlNodeTypes.attribute;
+
+      if (isAttribute) {
+        const attributeName = prop.name;
+        return !attributesToRemove.includes(attributeName);
+      }
+
+      const isDynamicAttribute = prop.name === 'bind';
+
+      if (isDynamicAttribute) {
+        const attributeName = prop.arg?.content;
+        return !attributesToRemove.includes(attributeName);
+      }
+
+      return true;
+    });
+  }
+}
+
+export function getDataCyTemplateOptions(mode) {
+  return {
+    compilerOptions: {
+      nodeTransforms: mode === PRODUCTION_MODE ? [removeDataTestAttrs] : [],
+    },
+  }
+}
+
+export default defineConfig(({ mode }) => ({
   resolve: {
     alias: {
       '~': `${path.resolve(__dirname)}`,
@@ -29,7 +70,7 @@ export default defineConfig({
   },
 
   plugins: [
-    vue(),
+    vue({ template: getDataCyTemplateOptions(mode) }),
     svgLoader(),
   ],
 
@@ -40,4 +81,4 @@ export default defineConfig({
   preview: {
     port: 8081,
   },
-});
+}));
