346a76d82c6f0d20485edca525bb6f499c73b1b9
YT8434 <58299318+YT8434@users.noreply.github.com>
2023-09-08 12:56:36 +0200

    feat: backup and restore local storage to/from a file on the client (#71)

--- core/components/Dev.vue
+++ core/components/Dev.vue
@@ -2,6 +2,7 @@
 .host-bar {
   width: 256px;
 }
+
 .host-nav {
   color: #94979e;
   background: #f5f5f5;
@@ -27,11 +28,9 @@
             <div class="text-lg mb-4 pb-2 border-b">Saved Configurations</div>
             <div v-for="name in settings.configurations">
               <div class="flex items-center mb-2 px-2">
-                <div class="w-1 h-6 bg-gray-600 mr-2"
-                     :class="{ 'bg-green-600': name === settings.currentConfig }" />
-                <button class="flex-grow text-left"
-                        :class="{ 'font-bold': name === settings.currentConfig }"
-                        @click="onLoad(name)">{{ name }}
+                <div class="w-1 h-6 bg-gray-600 mr-2" :class="{ 'bg-green-600': name === settings.currentConfig }" />
+                <button class="flex-grow text-left" :class="{ 'font-bold': name === settings.currentConfig }"
+                  @click="onLoad(name)">{{ name }}
                 </button>
                 <button class="flex items-center justify-center bg-gray-500 w-6 h-6" @click="deleteConfiguration(name)">
                   <CIcon type="trash" size="md" />
@@ -45,18 +44,28 @@
             <input id="dev__autosave" type="checkbox" v-model="settings.autosave" />
           </div>
 
-          <input class="w-full px-2 py-2 text-black outline-none text-sm"
-                 v-model="configNameInputVal"
-                 @keydown.enter="onSave"
-                 placeholder="Save as">
+          <input class="w-full px-2 py-2 text-black outline-none text-sm" v-model="configNameInputVal"
+            @keydown.enter="onSave" placeholder="Save as">
 
           <div class="dev__buttons flex my-3">
-
             <div class="w-1/2 pr-1">
               <CButton class="w-full  transition duration-150 btn__save" color="sky" @click="onSave">Save</CButton>
             </div>
             <div class="w-1/2 pl-1">
               <CButton class="w-full transition duration-150" color="sky" @click="onClear">Clear</CButton>
+            </div>
+          </div>
+          <div class="dev__buttons flex my-3">
+            <div class="w-1/2 pr-1">
+              <label>
+                <input @change="onRestoreFromFile" type="file" ref="uploadfile" hidden />
+                <CButton class="w-full  transition duration-150 btn__upload" color="sky" @click="$refs.uploadfile.click()">Restore
+                </CButton>
+              </label>
+            </div>
+            <div class="w-1/2 pl-1">
+              <CButton class="w-full  transition duration-150 btn__download" color="sky" @click="onDownLoadFile">Backup
+              </CButton>
             </div>
           </div>
 
@@ -89,6 +98,7 @@ const {
   saveConfiguration,
   deleteConfiguration,
   clearStorage,
+  localStorageBackup,
 } = useMock();
 
 const configNameInputVal = ref('');
@@ -102,6 +112,31 @@ async function onSave() {
   saveConfiguration(configName, newState);
 
   configNameInputVal.value = '';
+}
+
+async function onDownLoadFile() {
+  const appname = $host.getMeta().configurator.name;
+  localStorageBackup(appname);
+}
+
+function onRestoreFromFile(e) {
+  const f = e.target.files[0]
+  if (f) {
+    var reader = new FileReader();
+    reader.onload = function (e) {
+      var text = e.target.result;
+      var backup = JSON.parse(text);
+      for (var key in backup) {
+        var value = decodeURIComponent(unescape(backup[key]));
+        window.localStorage.setItem(key, value);
+      }
+    };
+    reader.readAsText(f);
+  } else {
+    alert('Failed to load file');
+  }
+
+  window.location.reload();    
 }
 
 function onLoad(name) {
