5860b4cf5543085658bd49c0471ee3e3b545cfb8
mattbarton44 <mattbarton44@gmail.com>
2023-09-19 12:34:50 +0100

    feat: create panel component

---
+++ core/components/Panel.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="" :class="widthClass">
+    test
+  </div>
+</template>
+
+<script setup>
+import App from './App.vue';
+import { useRootStore } from '../store';
+
+const props = defineProps({
+  expanded: String,
+  collapsed: String,
+  panelId: String,
+});
+
+const store = useRootStore();
+
+
+
+const widthClass = computed(() => {
+  if(store.panels[panelId].isExpanded) {
+    return props.expanded;
+  }
+  return props.collapsed;
+})
+
+// need to update collapsed/expanded in a store for the panelId
+
+
+
+
+</script>
