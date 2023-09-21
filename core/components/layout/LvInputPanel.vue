47af26642657cc0fb93e89bc3fb424ea37a77f0c
mattbarton44 <mattbarton44@gmail.com>
2023-09-21 12:29:01 +0100

    chore: push to pull

--- core/components/layout/LvInputPanel.vue
+++ core/components/layout/LvInputPanel.vue
@@ -1,12 +1,23 @@
 <template>
   <LvPanel :expanded="550" panelId="input">
-    <slot>
-      <div>input</div>
-    </slot>
+    <div class="bg-indigo-light w-full h-full">
+      <div class="flex flex-grow w-full h-full " :class="{ 'flex-col rotate-180' :!store.panels['input'].isExpanded }" >
+        <div v-for="tab in tabs" class="text-center bg-steel-light w-full h-full flex justify-center cursor-pointer">
+          <div class="" :class="{ '[writing-mode:vertical-lr]' :!store.panels['input'].isExpanded }">{{ tab }}</div>
+        </div>
+      </div>
+      <div v-if="store.panels['input'].isExpanded">content</div>
+    </div>
   </LvPanel>
 </template>
 
 <script setup>
 import LvPanel from '../ui/LvPanel.vue';
+import { useLeviateStore } from '../../store/leviate';
+
+const tabs = ['Input'];
+
+const store = useLeviateStore();
 
 </script>
+
