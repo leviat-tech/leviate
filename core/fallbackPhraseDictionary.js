96f5aa7029b11f11020e575a5e89ea0b498af50b
reeves999999 <55240533+reeves999999@users.noreply.github.com>
2024-04-25 10:35:01 +0100

    chore: revised

---
+++ core/fallbackPhraseDictionary.js
@@ -0,0 +1,4 @@
+export const fallbackPhraseDictionary = {
+  warning_app_mismatch:
+    'This design was created in an earlier version of the software. Please see: ',
+};
--- core/plugins/localize.js
+++ core/plugins/localize.js
@@ -1,44 +1,31 @@
 import { get } from 'lodash-es';
 import { ref } from 'vue';
 import logger from '../extensions/logger';
+import { fallbackPhraseDictionary } from '../fallbackPhraseDictionary';
 
 const locale = ref('en');
 let dictionary = {};
 let availableLanguages = [];
 
-let defaultDictionary = {
-  en: {
-    warning_app_mismatch:
-      'This design was created in an earlier version of the software. Please see: ',
-  },
-  de: {
-    warning_app_mismatch:
-      'Dieses Design wurde in einer früheren Version der Software erstellt. Bitte sehen: ',
-  },
-  fr: {
-    warning_app_mismatch:
-      "Cette conception a été créée dans une version antérieure du logiciel. S'il te plait regarde: ",
-  },
-  it: {
-    warning_app_mismatch:
-      'Questo disegno è stato creato in una versione precedente del software. Perfavore guarda: ',
-  },
-  pl: {
-    warning_app_mismatch:
-      'Projekt ten powstał we wcześniejszej wersji oprogramowania. Proszę zobaczyć: ',
-  },
-};
-
 function localize(phrase, options = {}) {
   const capitalize = (string) =>
     string.replace(/(^|\s)\S/g, (l) => l.toUpperCase());
-  const translation =
-    get(dictionary, [locale.value, phrase]) || options.default;
+  let translation = get(dictionary, [locale.value, phrase]) || options.default;
 
   if (translation === undefined) {
-    console.error(`Unable to translate ${phrase}`);
-    return `{${phrase}}`;
+    const fallback = fallbackPhraseDictionary[phrase];
+    let error = `Unable to translate ${phrase}`;
+    if (fallback) {
+      translation = fallback;
+      console.error(
+        `${error}, English fallback phrase "${fallback}" used instead.`
+      );
+    } else {
+      console.error(error);
+      return `{${phrase}}`;
+    }
   }
+
   return options.capitalize ? capitalize(translation) : translation;
 }
 
@@ -65,15 +52,7 @@ export const useLocalize = () => {
 
 export default {
   install(app, { locales }) {
-    const localeKeys = Object.keys(locales);
-
-    //remove defaultDictionary keys not in locales and combine both
-    dictionary = Object.keys(defaultDictionary).reduce((iterator, key) => {
-      if (localeKeys.includes(key))
-        iterator[key] = { ...locales[key], ...defaultDictionary[key] };
-      return iterator;
-    }, {});
-
+    Object.assign(dictionary, locales);
     availableLanguages = Object.keys(dictionary);
 
     Object.assign(app.config.globalProperties, {
