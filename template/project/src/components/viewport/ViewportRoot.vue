b002a46e5b40bcc89d924c6ec26469a8d43ad4cb
danielstorey <danielstorey@gmail.com>
2024-08-16 12:30:45 +0100

    fix: validation pane

--- template/project/src/components/viewport/ViewportRoot.vue
+++ template/project/src/components/viewport/ViewportRoot.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="viewport flex flex-col flex-1 h-full">
+  <div class="viewport relative flex flex-col flex-1 h-full">
     <div class="flex flex-none w-full items-center h-12 px-1 border-b">
       <ViewportToolbar />
     </div>
     <ViewportDrawing />
-    <LvValidationPane />
+    <LvValidationPane :messages="messageStore.configErrors" auto-appear/>
   </div>
 </template>
 
@@ -12,5 +12,14 @@
 <script setup>
 import ViewportToolbar from './ViewportToolbar.vue';
 import ViewportDrawing from './ViewportDrawing.vue';
-import { LvValidationPane } from '@crhio/leviate/components'
+import { LvValidationPane } from '@crhio/leviate/components';
+import { useMessageStore } from '@crhio/leviate';
+
+const messageStore = useMessageStore();
+
+// setTimeout(() => {
+//
+// messageStore.setConfigError('This is a calculation error message')
+// messageStore.setConfigWarning('This is a calculation error warning')
+// }, 2000);
 </script>
