024aa79125dcc5454e489958fa198180d3c6e6e0
mattbarton44 <mattbarton44@gmail.com>
2024-09-05 11:45:18 +0100

    chore: init branch setup

---
+++ template/project/src/views/Import.vue
@@ -0,0 +1,13 @@
+<template>
+  <LvLayout>
+    <div class="h-full flex items-center justify-center">
+       {{ $l('import') }}
+       <ImportTest />
+    </div>
+  </LvLayout>
+</template>
+
+<script setup>
+import LvLayout from '@crhio/leviate/components/layout/LvLayout.vue';
+import ImportTest from '@crhio/leviate/components/import/test.vue';
+</script>
