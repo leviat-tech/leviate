089a60dbe50fa9a52ac8033bfd2f7937213d0638
Yury Aleksiaichuk <yaleksiaichuk@crh.com>
2024-09-30 10:44:37 +0200

    feat: country translation added

--- core/components/layout/project/info/LvAddress.vue
+++ core/components/layout/project/info/LvAddress.vue
@@ -1,15 +1,29 @@
 <template>
   <address class="not-italic">
     <template v-for="field in fields">
-      <span :class="boldFields.includes(field) && 'font-bold'">{{ data[field] }}</span><br>
+      <span :class="boldFields.includes(field) && 'font-bold'">
+        {{ field === 'country' ? getCountryValue(data[field]) :  data[field] }}
+      </span><br>
     </template>
   </address>
 </template>
 
 <script setup>
+
+import { useLocalize } from '../../../../plugins/localize';
+
+const { $l } = useLocalize();
+
 const props = defineProps({
   data: Object,
   fields: Array,
   boldFields: { type: Array, default: [] },
 });
+
+function getCountryValue(country) {
+  if (country.includes('_')) {
+      return $l(country);
+    }
+      return country;
+};
 </script>
