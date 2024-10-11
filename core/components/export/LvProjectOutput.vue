2c6d5f6a32bf896794562d59b65d9a56912ed87f
Kamil Wasilonek <kamilwasilonek@griddynamics.com>
2024-10-11 10:58:12 +0200

    fix: add export components

---
+++ core/components/export/LvProjectOutput.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="p-4">
+    <CFormSection :title="$L('generate')">
+      <div class="flex gap-4">
+        <CIcon type="information-circle" size="md" />
+        <span>{{ $l('generate_instructions') }}</span>
+      </div>
+    </CFormSection>
+  </div>
+</template>
---
+++ core/components/layout/LvProjectRoot.vue
@@ -0,0 +1,13 @@
+<template>
+  <LvProjectPanelItem v-for="(component, name) in projectPanelItemsMap" :key="name" :name="name">
+    <component :is="component" />
+  </LvProjectPanelItem>
+</template>
+
+<script setup lang="ts">
+  import { LvProjectPanelItem } from '@crhio/leviate/components';
+
+  defineProps({
+    projectPanelItemsMap: Object,
+  });
+</script>
