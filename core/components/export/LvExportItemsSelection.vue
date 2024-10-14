0243789bd651ed1c5f5a798ee9dceac04bd41f61
Kamil Wasilonek <kamilwasilonek@griddynamics.com>
2024-10-14 14:50:19 +0200

    fix: pr comments

--- core/components/export/LvExportItemsSelection.vue
+++ core/components/export/LvExportItemsSelection.vue
@@ -2,21 +2,18 @@
   <div class="my-6">
     <div v-if="warningMessage?.length" class="flex p-2 bg-warning-lightest align-items">
       <CIcon type="warning" size="lg" class="mr-4 text-danger"></CIcon>
-      {{ $l(warningMessage) }}
+      {{ warningMessage }}
     </div>
   </div>
 
-  <div class="text-sm mb-2 font-semibold">{{ $L(label) }}</div>
+  <div class="text-sm mb-2 font-semibold">{{ label || $l('position_selection') }}</div>
 
   <slot />
 </template>
 
 <script setup lang="ts">
   defineProps({
-    label: {
-      type: String,
-      default: 'position_selection',
-    },
-    warningMessage: String
+    label: String,
+    warningMessage: String,
   });
 </script>
--- core/components/export/LvProjectOutput.vue
+++ core/components/export/LvProjectOutput.vue
@@ -1,10 +1,17 @@
 <template>
   <div class="p-4">
-    <CFormSection :title="$L('generate')">
+    <CFormSection :title="title || $L('generate')">
       <div class="flex gap-4">
         <CIcon type="information-circle" size="md" />
-        <span>{{ $l('generate_instructions') }}</span>
+        <span>{{ content || $l('generate_instructions')}}</span>
       </div>
     </CFormSection>
   </div>
 </template>
+
+<script lang="ts" setup>
+  defineProps({
+    title: String,
+    content: String,
+  });
+</script>
