47817f887b60ad1116f1c5e3670454522d325758
danielstorey <danielstorey@gmail.com>
2024-12-03 09:26:28 +0000

    chore: fix file drop input

--- core/components/import/LvInputFileDrop.vue
+++ core/components/import/LvInputFileDrop.vue
@@ -1,14 +1,19 @@
 <template>
   <div
-    @dragenter.prevent="active = true"
+    @mouseenter="isMouseActive = true"
+    @mouseleave="isMouseActive = false"
     :class="{ 'border-indigo bg-indigo bg-opacity-5' : active }"
     class="relative m-4 border-dashed border text-center justify-center rounded-sm p-4 border-gray-300 flex flex-col items-center gap-y-2 duration-150"
   >
-    <div v-if="true"
+
+    <!-- mask to prevent toggling drag state when a button is present -->
+    <div v-if="!isMouseActive"
+         @dragenter.prevent="active = true"
          @dragleave.prevent="active = false"
          @dragover.prevent
          @drop.prevent="onDrop"
-         class="absolute inset-0" />
+         class="absolute inset-0
+" />
     <slot />
   </div>
 </template>
@@ -19,6 +24,8 @@ import { ref } from 'vue';
 const emit = defineEmits(['select']);
 
 const active = ref(false);
+
+const isMouseActive = ref(false);
 
 function onDrop(e) {
   active.value = false;
