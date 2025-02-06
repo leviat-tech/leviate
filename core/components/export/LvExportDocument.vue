55dff57e3fa4e81465fb5c879c58d711876357a4
Kamil Wasilonek <kamilwasilonek@griddynamics.com>
2025-02-06 14:49:25 +0100

    fix: warnings for export

--- core/components/export/LvExportDocument.vue
+++ core/components/export/LvExportDocument.vue
@@ -7,21 +7,23 @@
       </div>
     </CButton>
 
-    <div v-if="isGenerated === false" class="flex items-center mt-4 text-danger font-bold">
-      <CIcon type="warning" size="md" class="mr-2"></CIcon>{{ $L('generate_failed') }}
-    </div>
-    <div v-if="isGenerated === true" class="flex items-center mt-4">
-      <CIcon type="check" size="md" class="text-success mr-2"></CIcon>
-      <span class="text-success font-bold">{{ $l('generate_success') }}</span>
-    </div>
+    <template v-if="!isGenerating">
+      <div v-if="isGenerated === false" class="flex items-center mt-4 text-danger font-bold">
+        <CIcon type="warning" size="md" class="mr-2"></CIcon>{{ $L('generate_failed') }}
+      </div>
+      <div v-if="isGenerated === true" class="flex items-center mt-4">
+        <CIcon type="check" size="md" class="text-success mr-2"></CIcon>
+        <span class="text-success font-bold">{{ $l('generate_success') }}</span>
+      </div>
+    </template>
   </div>
 </template>
 
 <script setup lang="ts">
-defineProps({
-  generate: Function,
-  isDisabled: Boolean,
-  isGenerated: Boolean,
-  isGenerating: Boolean,
-});
+  defineProps<{
+    generate: Function;
+    isDisabled: Boolean;
+    isGenerated: Boolean | null;
+    isGenerating: Boolean;
+  }>();
 </script>
