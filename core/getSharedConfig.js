3a0bb2ab19beaf1ee716010554377a61d5e45ffb
nmilasinovic94 <127110852+nmilasinovic94@users.noreply.github.com>
2025-03-04 10:01:28 +0100

    feat: load translations
    Co-authored-by: Milasinovic, Nikola <milasinovn@ae.com>

---
+++ core/getSharedConfig.js
@@ -0,0 +1,75 @@
+const path = require('path');
+const merge = require('lodash/merge');
+const vue = require('@vitejs/plugin-vue');
+const svgLoader = require('vite-svg-loader');
+const getDefaultTemplateCompilerOptions = require('./server/defaultTemplateCompilerOptions');
+const manifestPlugin = require('./server/manifestPlugin');
+const translationPlugin = require('./server/translation');
+
+/**
+ * Ensure page does a full reload when 3d files are updated
+ * as hot uploading not currently supported
+ * @return {{handleHotUpdate(*): void, name: string}}
+ */
+const threeReloadPlugin = () => ({
+  name: 'three-reload',
+  handleHotUpdate(ctx) {
+    if (ctx.file.search('3dmodel') > -1) {
+      ctx.server.ws.send({ type: 'full-reload' });
+    }
+  },
+});
+
+module.exports = function getSharedConfig({ mode, projectConfig = {} }) {
+  const sharedConfig = {
+    resolve: {
+      alias: {
+        '~': `${path.resolve(process.cwd())}`,
+        '@': `${path.resolve(process.cwd(), 'src')}`,
+        '@crhio/leviate': '/node_modules/@crhio/leviate/core',
+      },
+    },
+
+    base: './',
+
+    optimizeDeps: {
+      exclude: ['@headlessui/vue', '@crhio/leviate', '@crhio/normie', 'pinia', 'vue-router', 'vue'],
+      include: ['@crhio/leviate > axios', 'axios/lib/adapters/http'],
+    },
+
+    plugins: [
+      svgLoader(),
+      manifestPlugin(),
+      threeReloadPlugin(),
+      vue(getDefaultTemplateCompilerOptions(mode)),
+      translationPlugin()
+    ],
+
+    build: {
+      minify: false,
+    },
+
+    server: {
+      port: 8080,
+      proxy: {
+        '/api': {
+          target: process.env.SERVICE_URL,
+          headers: {
+            'x-service-key': process.env.SERVICE_KEY,
+          },
+          changeOrigin: true,
+        },
+      },
+    },
+
+    preview: {
+      port: 8081,
+    },
+
+    test: {
+      globals: true,
+    },
+  }
+
+  return merge(sharedConfig, projectConfig)
+}
