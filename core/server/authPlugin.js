7ee1c7b88865d752789f5cd30af912faefa4c59b
danielstorey <danielstorey@gmail.com>
2025-03-06 18:58:53 +0000

    chore: check for env vars in vite plugins

--- core/server/authPlugin.js
+++ core/server/authPlugin.js
@@ -7,19 +7,25 @@ module.exports = function tokenPlugin() {
   return {
     name: 'token-generator',
     configureServer(server) {
+      const { SERVICE_URL, LDS_CLIENT_ID, LDS_CLIENT_SECRET } = process.env;
+
+      if (!SERVICE_URL || !LDS_CLIENT_ID || !LDS_CLIENT_SECRET) {
+        throw new Error('please ensure you have the following environment variables set:\n- SERVICE_URL\n- LDS_CLIENT_ID\n- LDS_CLIENT_SECRET');
+      }
+
       server.middlewares.use('/auth/get-service-token', async(req, res, next) => {
-        const url = process.env.SERVICE_URL + '/auth/get-token';
+        const url = SERVICE_URL + '/auth/get-token';
         try {
-          const data = await fetch(url, {
+          const responseText = await fetch(url, {
             method: 'POST',
             body: JSON.stringify({
               grant_type: 'client_credentials',
-              client_id: process.env.LDS_CLIENT_ID,
-              client_secret: process.env.LDS_CLIENT_SECRET,
+              client_id: LDS_CLIENT_ID,
+              client_secret: LDS_CLIENT_SECRET,
             })
-          }).then(res => res.json());
+          }).then(res => res.text());
 
-          res.end(JSON.stringify(data));
+          res.end(responseText);
         } catch(e) {
           console.log('Error retrieving token:', e);
         }
