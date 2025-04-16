7adbfc06b53b937cc14ddd6aacdd7b9f3020c5de
danielstorey <danielstorey@gmail.com>
2025-04-16 14:52:17 +0100

    chore: fix syntax error and add interface

--- core/composables/useStateCompression.ts
+++ core/composables/useStateCompression.ts
@@ -1,8 +1,13 @@
 import { compressSync, decompressSync, strToU8, strFromU8 } from 'fflate';
 
+interface CompressedState {
+  _compressed: string,
+  [key: string]: any,
+}
+
 export default function useStateCompression() {
   return {
-    compress(state: any) {
+    compress(state: any): CompressedState {
       const stateStr: string = JSON.stringify(state);
 
       console.log('State size before compression:', stateStr.length);
@@ -13,11 +18,13 @@ export default function useStateCompression() {
 
       console.log('State size after compression:', stateStrCompressed.length);
 
+      // Explicitly set any existing state properties to undefined
+      // so they will be removed from the stored state, and replaced with compressed data
       return Object.keys(state).reduce((stateToSave, key) => {
         return { ...stateToSave, [key]: undefined };
-      }, { _compressed: stateStrCompressed }
+      }, { _compressed: stateStrCompressed });
     },
-    decompress(stateStrCompressed: string) {
+    decompress(stateStrCompressed: string): any {
       const buffer: Uint8Array = strToU8(stateStrCompressed, true)
       const decompressed: Uint8Array = decompressSync(buffer);
       const stateStr: string = strFromU8(decompressed);
