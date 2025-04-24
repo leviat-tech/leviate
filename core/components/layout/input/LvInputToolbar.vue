aaf83ea19f711c0fb745fa77c3d2e9a3bb696d04
Kamil Wasilonek <kamilwasilonek@griddynamics.com>
2025-04-24 15:19:53 +0200

    fix: formatting

--- core/components/layout/input/LvInputToolbar.vue
+++ core/components/layout/input/LvInputToolbar.vue
@@ -25,7 +25,8 @@
           'border-b-[3px] border-b-indigo text-indigo': isExpanded && activeTab === getTabName(tab),
           'border-b-[3px] border-b-transparent': isExpanded && activeTab !== getTabName(tab),
           'border-r-4 border-r-indigo text-indigo': !isExpanded && activeTab === getTabName(tab),
-          'disabled:cursor-not-allowed disabled:text-gray-300': typeof tab === 'object' && tab.disabled,
+          'disabled:cursor-not-allowed disabled:text-gray-300':
+            typeof tab === 'object' && tab.disabled,
         }"
       >
         <LvTabText :is-expanded="isExpanded">{{ $L(snakeCase(getTabName(tab))) }}</LvTabText>
@@ -35,61 +36,61 @@
 </template>
 
 <script setup lang="ts">
-  import { computed } from 'vue';
-  import { snakeCase } from 'lodash-es';
-  import { useRoute, useRouter } from 'vue-router';
+import { computed } from 'vue';
+import { snakeCase } from 'lodash-es';
+import { useRoute, useRouter } from 'vue-router';
 
-  import LvTabText from '../../ui/LvTabText.vue';
-  import { PanelNames, useLeviateStore } from '../../../store/leviate';
+import LvTabText from '../../ui/LvTabText.vue';
+import { PanelNames, useLeviateStore } from '../../../store/leviate';
 
-  const route = useRoute();
-  const router = useRouter();
+const route = useRoute();
+const router = useRouter();
 
-  type Tab = string | { name: string; disabled: boolean };
+type Tab = string | { name: string; disabled: boolean };
 
-  const props = withDefaults(
-    defineProps<{
-      tabs: Array<Tab>;
-      panel?: PanelNames;
-    }>(),
-    {
-      tabs: () => ['input'],
-      panel: 'input',
-    }
-  );
+const props = withDefaults(
+  defineProps<{
+    tabs: Array<Tab>;
+    panel?: PanelNames;
+  }>(),
+  {
+    tabs: () => ['input'],
+    panel: 'input',
+  }
+);
 
-  const store = useLeviateStore();
-  const activePanel: PanelNames = props.panel;
+const store = useLeviateStore();
+const activePanel: PanelNames = props.panel;
 
-  const isExpanded = computed(() => store.panels[activePanel].isExpanded);
-  const activeTab = computed(() => store.panels[activePanel].activeTab);
+const isExpanded = computed(() => store.panels[activePanel].isExpanded);
+const activeTab = computed(() => store.panels[activePanel].activeTab);
 
-  const clickTab = (tabId) => {
-    store.panels[activePanel].activeTab = tabId;
+const clickTab = (tabId) => {
+  store.panels[activePanel].activeTab = tabId;
 
-    if (!store.panels.input.isExpanded) {
-      store.panels[activePanel].isExpanded = true;
-    }
-
-    const query = { ...route.query };
-
-    if (activePanel === 'input') {
-      query.inputTab = tabId;
-    } else {
-      query.resultTab = tabId;
-    }
-    router.push({ query });
-  };
-
-  if (props.tabs.length > 0 && !props.tabs.includes(activeTab.value)) {
-    clickTab(props.tabs[0]);
+  if (!store.panels.input.isExpanded) {
+    store.panels[activePanel].isExpanded = true;
   }
 
-  const getTabName = (tab: Tab): string => {
-    if (typeof tab === 'object' && tab.name) {
-      return tab.name;
-    }
+  const query = { ...route.query };
 
-    return tab as string;
-  };
+  if (activePanel === 'input') {
+    query.inputTab = tabId;
+  } else {
+    query.resultTab = tabId;
+  }
+  router.push({ query });
+};
+
+if (props.tabs.length > 0 && !props.tabs.includes(activeTab.value)) {
+  clickTab(props.tabs[0]);
+}
+
+const getTabName = (tab: Tab): string => {
+  if (typeof tab === 'object' && tab.name) {
+    return tab.name;
+  }
+
+  return tab as string;
+};
 </script>
