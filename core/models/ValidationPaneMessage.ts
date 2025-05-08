0dfe8d1af961db544f62fb905b53c99282ba8299
Kamil Wasilonek <kamilwasilonek@griddynamics.com>
2025-05-08 16:20:57 +0200

    fix: types

--- core/models/ValidationPaneMessage.ts
+++ core/models/ValidationPaneMessage.ts
@@ -1,7 +1,7 @@
 export default interface ValidationPaneMessage {
     id: string,
     content: string,
-    timestamp: number,
-    isDismissable: boolean,
+    timestamp?: number,
+    isDismissable?: boolean,
     type: 'danger' | 'warning' | 'success' | 'magic' | 'info' | 'default';
 }
