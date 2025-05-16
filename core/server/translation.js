52049f8b36251b45dd869e0075dc26deeccb1af0
Kamil Wasilonek <kamilwasilonek@griddynamics.com>
2025-05-16 11:17:35 +0200

    fix: translation key check

--- core/server/translation.js
+++ core/server/translation.js
@@ -3,18 +3,23 @@ const axios = require('axios');
 const merge = require('lodash/merge');
 
 const translationUrl = process.env.VITE_TRANSLATE_BASE_URL;
+
+if (!translationUrl) {
+  throw new Error('Please ensure environment variable VITE_TRANSLATE_BASE_URL is defined.');
+}
+
 const localesFolderName = 'src/locales';
 
 module.exports = async function translationPlugin() {
   const { translationName } = fs.readJsonSync(`${process.cwd()}/manifest.json`);
-  
+
   try {
     if (!fs.existsSync(localesFolderName)) {
       fs.mkdirSync(localesFolderName);
     }
     if (!translationName.length) {
       throw new Error(
-        'Make sure that you specify the project name in order to get translations keys',
+        'Make sure that you specify the project name in order to get translations keys'
       );
     }
   } catch (err) {
@@ -24,11 +29,12 @@ module.exports = async function translationPlugin() {
   let jsonData = {};
   const fileName = `${localesFolderName}/index.json`;
   const languages_data = await axios.get(translationUrl);
-  
-  const languages = languages_data.data.filter(app => app.name === translationName)[0].dictionaries;
-  const countries = languages_data.data.filter(app => app.name === 'Countries')[0].dictionaries;
 
-  Object.keys(languages).forEach(translation => {
+  const languages = languages_data.data.filter((app) => app.name === translationName)[0]
+    .dictionaries;
+  const countries = languages_data.data.filter((app) => app.name === 'Countries')[0].dictionaries;
+
+  Object.keys(languages).forEach((translation) => {
     if (Object.keys(languages[translation]).length !== 0) {
       jsonData = {
         ...jsonData,
@@ -37,4 +43,4 @@ module.exports = async function translationPlugin() {
     }
   });
   fs.writeFileSync(fileName, JSON.stringify(jsonData));
-}
+};
