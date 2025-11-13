0fdac5dbe70c1809e9cede8362cb2ea34f693c4f
danielstorey <danielstorey@gmail.com>
2025-11-13 10:56:49 +0000

    chore: normalise banner styling

--- core/components/layout/LvReadOnlyBanner.vue
+++ core/components/layout/LvReadOnlyBanner.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="bg-yellow-100 text-yellow-800 text-sm p-2 text-center">
+    <div class="bg-yellow-100 text-yellow-800 border-b border-yellow-500 text-sm p-2 text-center">
         {{ $l("read_only_banner") }}
     </div>
 </template>
