1e09fa27d55a762a56451d0906b9a55be084a432
danielstorey <danielstorey@gmail.com>
2025-11-21 11:12:44 +0000

    fix: ensure pasted state is persisted
    fix: restore navigation when pasting design

--- core/composables/useDesignCopyPaste.ts
+++ core/composables/useDesignCopyPaste.ts
@@ -1,55 +1,64 @@
 import { useToast } from './useToast';
-import { useRootStore } from '../store';
+import { transact, useRootStore } from '../store';
 import { useMeta } from '../plugins/host';
 import useEnvironment from './useEnvironment';
+import { useRoute, useRouter } from 'vue-router';
 
 function getAppName() {
   return useMeta().configurator.name;
 }
 
-function copyDesign() {
-  const state = useRootStore().toJSON();
-  state.appName = getAppName();
-  const stateStr = JSON.stringify(state, null, 2);
-  navigator.clipboard.writeText(stateStr);
-  useToast().showToast('Design copied to clipboard. Use Ctrl+ Alt + Shift + V to load.');
-}
-
-async function pasteDesign() {
-  let error = null;
-  const stateStr = await navigator.clipboard.readText();
-  const appName = getAppName();
-
-  try {
-    const state = JSON.parse(stateStr);
-
-    if (state.appName !== appName) {
-      throw new Error('Mismatching application name');
-    }
-
-    const rootStore = useRootStore();
-
-    if (rootStore.serialization_version !== state.serialization_version) {
-      console.warn('State serialization version does not match the current application version. Unexpected results may occur');
-    }
-
-    const isConfirmed = confirm('This will replace the design with the contents of the clipboard and all previous data will be lost. Are you sure?');
-
-    if (isConfirmed) {
-      const { appName, ...stateToLoad } = state;
-      rootStore.replaceState(stateToLoad);
-    }
-  } catch (e) {
-    console.error(e);
-    error = `Could not read design from Clipboard. Please check that the clipboard content is a valid ${appName} design`;
-  }
-
-  if (error !== null) {
-    useToast().showToast(error, { type: 'error' });
-  }
-}
-
 export function useDesignCopyPaste() {
+  const route = useRoute();
+  const router = useRouter();
+
+  function copyDesign() {
+    const state = useRootStore().toJSON();
+    state.appName = getAppName();
+    const stateStr = JSON.stringify({ state, currentRoute: route }, null, 2);
+    navigator.clipboard.writeText(stateStr);
+    useToast().showToast('Design copied to clipboard. Use Ctrl+ Alt + Shift + V to load.');
+  }
+
+  async function pasteDesign() {
+    let error = null;
+    const stateStr = await navigator.clipboard.readText();
+    const appName = getAppName();
+
+    try {
+      const { state, currentRoute } = JSON.parse(stateStr);
+
+      if (state.appName !== appName) {
+        throw new Error('Mismatching application name');
+      }
+
+      const rootStore = useRootStore();
+
+      if (rootStore.serialization_version !== state.serialization_version) {
+        console.warn('State serialization version does not match the current application version. Unexpected results may occur');
+      }
+
+      const isConfirmed = confirm('This will replace the design with the contents of the clipboard and all previous data will be lost. Are you sure?');
+
+      if (isConfirmed) {
+        const { appName, ...stateToLoad } = state;
+        await router.replace('/');
+
+        transact('Paste state', () => {
+          rootStore.replaceState(stateToLoad);
+          return router.replace(currentRoute);
+        });
+      }
+    } catch (e) {
+      console.error(e);
+      error = `Could not read design from Clipboard. Please check that the clipboard content is a valid ${appName} design`;
+    }
+
+    if (error !== null) {
+      useToast().showToast(error, { type: 'error' });
+    }
+  }
+
   window.addEventListener('keydown', async (e) => {
     const isGlobalKeyActive = e.shiftKey && e.ctrlKey && e.altKey;
 
